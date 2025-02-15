2c145373e66185fc4152eb891683a9c73ad7e341
EnzoEmir <164296530+EnzoEmir@users.noreply.github.com>
2025-02-15 04:07:58 -0300

    Outiliers Certo

--- DataProcessing_RBPi/RBPi/utils/config.py
+++ DataProcessing_RBPi/RBPi/utils/config.py
@@ -1,4 +1,4 @@
 # Configurações gerais
 SERVER_URL = "http://localhost:8000/api/receive_data"  # URL para onde os dados serão enviados
 BATCH_SIZE = 10  # Número de leituras antes de salvar os dados em um novo arquivo CSV
-READ_INTERVAL = 1  # Intervalo de tempo entre as leituras (em segundos)
+READ_INTERVAL = 0.1  # Intervalo de tempo entre as leituras (em segundos)
